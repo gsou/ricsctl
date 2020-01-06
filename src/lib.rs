54f730f6702f1dcb5b1542cc4f85506aa01dede7
Skami <guillaume.soulard.1@ulaval.ca>
2020-01-06 13:16:27 -0500

    Library

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+extern crate clap;
+extern crate env_logger;
+extern crate protobuf;
+#[macro_use] extern crate log;
+extern crate serialport;
+extern crate libloading;
+extern crate libc;
+extern crate rlua;
+
+pub mod script;
+pub mod server;
+pub mod rics;
+pub mod host;
