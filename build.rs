3bb217807b8bc57a5b34686b1e8c1103b9aaf92c
Skami <gsou859@gmail.com>
2020-10-26 23:57:16 -0400

    Disable build.rs for now

--- build.rs
+++ build.rs
@@ -5,12 +5,12 @@ extern crate protoc_rust;
 use protoc_rust::Customize;
 
 fn main() {
-	protoc_rust::run(protoc_rust::Args {
-	    out_dir: "src",
-	    input: &["rics.proto"],
-	    includes: &[],
-	    customize: Customize {
-	        ..Default::default()
-	    },
-	}).expect("protoc");
+	//protoc_rust::run(protoc_rust::Args {
+	//    out_dir: "src",
+	//    input: &["rics.proto"],
+	//    includes: &[],
+	//    customize: Customize {
+	//        ..Default::default()
+	//    },
+	//}).expect("protoc");
 }
