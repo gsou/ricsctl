b07e93d99bf3410156f97f25535585da02c9a8dd
Skami <gsou859@gmail.com>
2020-11-25 11:09:42 -0500

    get_time_ms api function

--- src/script.rs
+++ src/script.rs
@@ -121,6 +121,11 @@ impl rlua::UserData for ServerBox {
             }
             Ok(())
         });
+
+        methods.add_method("get_time_ms", |_, this, () | {
+            Ok( std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).expect("Invalid current time").as_millis() )
+        });
+
     }
 }
 
